e3d95451b7600fb874a50e33efe7600e4094061c
Aryan Bhandari <aryanbhandari4077@gmail.com>
2025-11-13 11:42:05 +0530

    Backend url used

---
+++ dist/assets/ManageUsers-DEtr8p0Z.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BwMterpV.js";import{S as t}from"./StudentsTable-CaJeOabD.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
--- dist/assets/ManageUsers-r6AlBbEX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DrrpgQNc.js";import{S as t}from"./StudentsTable-9sdNsHEc.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
