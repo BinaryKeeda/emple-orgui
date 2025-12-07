80142357e0cc892a0e2d92286d08902efea049f5
Aryan Bhandari <aryanbhandari@Aryans-MacBook-Air.local>
2025-12-07 22:33:36 +0530

    sa

---
+++ dist/assets/Login-DfFLcuZ_.js
@@ -0,0 +1,1 @@
+import{r as o,B as e,j as t}from"./index-gwSlh4W6.js";function a(){return o.useEffect(()=>{window.location.href=`${String(e).includes("binarykeeda.com")?"https://login.binarykeeda.com":"http://localhost:5175"}`},[]),t.jsx(t.Fragment,{})}export{a as default};
