4dfde9592aa85be64c1865b042e89b120f957961
Aryan Bhandari <aryanbhandari@Aryans-MacBook-Air.local>
2025-12-07 14:52:11 +0530

    Admin ui push

--- dist/assets/ManageUsers-CjSYDxYG.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DVvBISA5.js";import{S as t}from"./StudentsTable-BEmeKKIr.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
---
+++ dist/assets/ManageUsers-jUYLTLHg.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C8FLH89x.js";import{S as t}from"./StudentsTable-CUgEgseO.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
