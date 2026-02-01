562846b1542aef2a308c3b4194edc48616770794
Aryan <aryan@rival.io>
2026-02-01 14:57:41 +0530

    Mode fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
 
-  const isProd = env.VITE_MODE === 'production';
 
   return {
     plugins: [react(), tailwindcss()],
-    base: isProd ? '/campus-admin/' : '/',
+    base: '/campus-admin/' ,
     server: {
       port: 5175,
       open: true,
